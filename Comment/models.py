7d555445722024ea475553c80c2b12df256ad62a
angry0957 <abdul.095722@gmail.com>
2018-04-29 12:00:53 +0500

    beforeUI

--- Comment/models.py
+++ Comment/models.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from User.models import User
+from Rooms.models import Room
 # Create your models here.
 class Comment(models.Model):
 	Desc = models.CharField(max_length=200)
 	userid = models.ForeignKey(User,on_delete=models.CASCADE)
+	roomid=models.ForeignKey(Room,on_delete=models.CASCADE)
 
 
 	def __str__(self):
