e2ab2a862a344abd6d28ab090fb8baf6e6cfc2d3
angry0957 <abdul.095722@gmail.com>
2018-04-29 12:18:43 +0500

    beforeUI

--- Comment/models.py
+++ Comment/models.py
@@ -8,7 +8,7 @@ from Rooms.models import Room
 class Comment(models.Model):
 	Desc = models.CharField(max_length=200)
 	userid = models.ForeignKey(User,on_delete=models.CASCADE)
-	roomid=models.ForeignKey(Room,on_delete=models.CASCADE)
+	roomid=models.ForeignKey(Room,on_delete=models.CASCADE,default=1)
 
 
 	def __str__(self):
